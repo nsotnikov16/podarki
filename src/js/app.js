b52b700a0e472f978d280a5e7c39ea7f8ffc6be4
Nikita <nsotnikov16@yandex.ru>
2022-04-01 17:33:45 +0500

    update

--- src/js/app.js
+++ src/js/app.js
@@ -274,8 +274,6 @@ if (aside) {
     })
 }
 
-
-
 // "Самые популярные"
 const swiperPopular = document.querySelector('.swiper.popular')
 if (swiperPopular) {
@@ -316,7 +314,6 @@ if (forms.length > 0) {
     forms.forEach(form => {
         const elements = Array.from(form.elements)
         const button = form.querySelector('button')
-        const phone = form.querySelector('#phone')
         if (elements.length > 0) {
             elements.forEach(item => {
                 if (['INPUT', 'TEXTAREA'].includes(item.tagName)) {
@@ -328,3 +325,67 @@ if (forms.length > 0) {
         }
     })
 }
+
+
+// Детальная страница
+const swiperThumbsDetail = new Swiper(".detail-swiper-thumbs", {
+    direction: 'vertical',
+    watchSlidesProgress: true,
+    navigation: {
+        nextEl: ".detail-swiper-thumbs .swiper-button-next",
+        prevEl: ".detail-swiper-thumbs .swiper-button-prev",
+    },
+    mousewheel: true,
+    breakpoints: {
+        1024: {
+            spaceBetween: 10,
+            slidesPerView: 4,
+        }
+    }
+});
+const swiperDetail = new Swiper(".detail-swiper", {
+    thumbs: {
+        swiper: swiperThumbsDetail,
+    },
+    autoplay: true,
+    mousewheel: true,
+    /*  pagination: {
+         el: ".swiper-lightbox .swiper-pagination",
+         type: "fraction",
+     }, */
+})
+
+
+// Скролл
+const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+    animationTime = 400,
+    framesCount = 30;
+
+anchors.forEach(function (item) {
+    // каждому якорю присваиваем обработчик события
+    item.addEventListener('click', function (e) {
+        // убираем стандартное поведение
+        e.preventDefault();
+
+        // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+        let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+        // запускаем интервал, в котором
+        let scroller = setInterval(function () {
+            // считаем на сколько скроллить за 1 такт
+            let scrollBy = coordY / framesCount;
+
+            // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+            // и дно страницы не достигнуто
+            if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+                // то скроллим на к-во пикселей, которое соответствует одному такту
+                window.scrollBy(0, scrollBy);
+            } else {
+                // иначе добираемся до элемента и выходим из интервала
+                window.scrollTo(0, coordY);
+                clearInterval(scroller);
+            }
+            // время интервала равняется частному от времени анимации и к-ва кадров
+        }, animationTime / framesCount);
+    });
+});
